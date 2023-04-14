a98da94b542cf3d14482f9f2bbd1c9137a93c243
Almazov-Artyom <125147917+Almazov-Artyom@users.noreply.github.com>
2023-04-15 02:02:35 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,9 +2,34 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T, int size>
+template<typename T, int size = 0>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+ private:
+    T arr[size] = {0};
+    int left, right, count;
+ public:
+    TPQueue(): left(0), right(0), count(0) {}
+    void push(const T& sm) {
+        if (count == 0) {
+            arr[0] = sm;
+            ++right;
+            ++count;
+        } else {
+            for (int i = right; i > left; --i) {
+                if (arr[(i - 1) % size].prior >= sm.prior) {
+                    arr[i % size] = sm;
+                    break;
+                } else {
+                    arr[i % size] = arr[(i - 1) % size];
+                    arr[(i - 1)%size] = sm;
+                }
+            }
+            ++right;
+        }
+    }
+    const T& pop() {
+        return arr[(left++ % size)];
+    }
 };
 
 struct SYM {
